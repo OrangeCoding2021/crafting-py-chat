00d3c414130a7cf033875d9c2fd7eabc7ae3c584
OrangeCoding2021 <gaffneyc.2021@gmail.com>
2022-08-10 12:33:25 -0400

    Documentation changes

--- iterateOverDirEntries.py
+++ iterateOverDirEntries.py
@@ -20,8 +20,9 @@ def fullSearch(term, path, dateStart=0, dateEnd=0):
     """
     # Get list of items in dir, for each item do the following
     for i in os.listdir(path):
-        # Validate file type
+        # Validate file type.
         if ".gz" in os.path.splitext(os.path.join(path, i))[-1]:
+            # If right type of file, search
             print("Searching... " + str(i))
             search(os.path.join(path, i), term)
 
@@ -35,6 +36,7 @@ def search(file, term):
     """
     try:
         linenum = 0
+        # Open file and output the lines the search term is found on
         with g.open((file), 'rt') as f:
             for line in f.readlines():
                 linenum += 1
@@ -43,6 +45,9 @@ def search(file, term):
 
     except g.BadGzipFile:
         print(str(file)+"is not a GZip file... trying open")
+        # Think about removing because of limited use case only really good for
+        # When a file in the directory is a text file already
+        # If it is not a gZip file tries opening file regularly
         try:
             linenum = 0
             with open(file, 'r') as f:
@@ -50,7 +55,7 @@ def search(file, term):
                 for line in f.readlines():
                     linenum += 1
                     if term in line:
-                        print(f"{term} found on line {linenum} " + line)
+                        print(f"{term} found on line {linenum} ")
 
         except:
             print("Open failed as well")
